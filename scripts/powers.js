35bfe6d4660d64a57830778afe777aab6ade53a0
Grayson Smith <graysonsmith91@gmail.com>
2022-12-16 12:01:13 -0600

    added html display function for powers.js

--- scripts/powers.js
+++ scripts/powers.js
@@ -1,1 +1,19 @@
-// powers module
+import { getPowers } from "./database.js";
+
+// create function to display html on 
+
+// make sure power name is set to name not size
+
+export const displayPowers = () => {
+    const powers = getPowers()
+    let html = "<ul>"
+    const listItems = powers.map(power => {
+        return `<li>
+            <input type="checkbox" name="power" value="${power.id}" /> ${power.name}
+        </li>`
+    })
+
+    html += listItems.join("")
+    html += "</ul>"
+    return html
+}
